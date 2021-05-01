7683ebf4314d395fa1199210dd29f0bbb1da9dae
D-liu <wenhaoliu1@hotmail.com>
2021-05-01 14:06:22 +0800

    引入了组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
+    <!-- 路由的出口 -->
     <router-view />
+    <div>
+      <i class="toutiao toutiao-wuwangluo"></i>
+      <van-button type="primary">主要按钮</van-button>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from './router'
 import store from './store'
 
 import './style/global.less'
+// 加载vant核心组件
+import Vant from 'vant'
+// 加载组件库全局样式
+import 'vant/lib/index.css'
+
+// import之间除了import 不能有其它代码
+
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
